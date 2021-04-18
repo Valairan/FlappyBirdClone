ba6bd763110e941fc22c3a720dee0e1c5db26ae2
valairan <darylvalairan@outlook.com>
2021-04-18 22:38:45 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,236 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const bird = document.querySelector(".bird");
+  const gameDisplay = document.querySelector(".game-container");
+  const sky = document.querySelector(".sky");
+  const groundContainer = document.querySelector(".ground-container");
+  const timer = document.querySelector(".timer-text");
+  const score = document.querySelector(".score-text");
+
+
+  gameDisplay.addEventListener('click', function (e) {
+
+    if (!isGameOver) {
+
+      if (jumpCond === false) {
+        if (birdBottom < 470)
+          jump();
+      }
+
+    }
+  });
+
+  let elementContainer = [];
+  const backgroundArray = ["Beach", "Capital", "Krakow", "Lake", "Mountain"];
+  const obstacles = [
+    "Dragon.gif",
+    "Eagle.gif",
+    "FootballPlayer.gif",
+    "PolishFlag.png",
+    "Shamrock.png",
+    "Siren.gif",
+  ];
+
+  const isOverlapping = (e1, secondDiv) => {
+
+    e2 = secondDiv.mdiv;
+    if (e1.length && e1.length > 1) {
+      e1 = e1[0];
+    }
+    if (e2.length && e2.length > 1) {
+      e2 = e2[0];
+    }
+    const rect1 = e1 instanceof Element ? e1.getBoundingClientRect() : false;
+    const rect2 = e2 instanceof Element ? e2.getBoundingClientRect() : false;
+
+    let overlap = false;
+
+    if (rect1 && rect2) {
+      overlap = !(
+        rect1.right < rect2.left ||
+        rect1.left > rect2.right ||
+        rect1.bottom < rect2.top ||
+        rect1.top > rect2.bottom
+      );
+
+      return { number: secondDiv.number, overlapState: overlap };
+    }
+
+
+    console.warn('Please provide valid HTMLElement object');
+    return { number: 25, overlapState: overlap };
+  }
+  const timerJump = ms => new Promise(res => setTimeout(res, 0.001))
+  let currentlyColliding = false;
+  let jumpCond = false;
+  let birdBottom = 250;
+  let birdLeft = 100;
+  let isGameOver = false;
+
+  let gravity = 2;
+  let time = 60;
+  let scoreCount = 0;
+
+  timer.innerHTML = time;
+  score.innerHTML = scoreCount;
+
+  const startGame = () => {
+    birdBottom -= gravity;
+    bird.style.left = birdLeft + "px";
+    bird.style.bottom = birdBottom + "px";
+    gravity += 0.3;
+  };
+
+  let gameTimerId = setInterval(startGame, 20);
+
+  const jump = () => {
+    jumpCond = true;
+    nexPosY = birdBottom + 75;
+    load();
+    gravity = 2;
+    jumpCond = false;
+  };
+  async function load() { // We need to wrap the loop into an async function for this to work
+    bird.style.backgroundImage = "url(LeprechaunJumpGIF.gif)"
+    while (birdBottom < nexPosY) {
+
+      gravity = 0;
+      birdBottom += 2;
+      await timerJump(3); // then the created Promise can be awaited
+    }
+    bird.style.backgroundImage = "url(LeprechaunFallGIF.gif)"
+  }
+
+  const control = (e) => {
+    if (e.keyCode === 32) {
+      jump();
+    }
+  };
+
+  const generateObstacle = () => {
+    let obstacleLeft = 400;
+    let randomHeight = 62 + Math.random() * 380;
+    let obstacleBottom = randomHeight;
+
+    const obstacle = document.createElement("div");
+
+    if (!isGameOver) {
+      obstacle.classList.add("obstacle");
+    }
+
+    let obstacleNumber = Math.floor(Math.random() * 6);
+
+    gameDisplay.appendChild(obstacle);
+
+    obstacle.style.backgroundImage = `url('./obstacle/${obstacles[obstacleNumber]}')`;
+
+    obstacle.style.left = obstacleLeft + "px";
+
+    obstacle.style.bottom = obstacleBottom + "px";
+
+    elementContainer.push({ number: obstacleNumber, mdiv: obstacle })
+
+    const moveObstacle = () => {
+      obstacleLeft -= 2;
+      obstacle.style.left = obstacleLeft + "px";
+
+      if (obstacleLeft === -10) {
+        clearInterval(timerId);
+        gameDisplay.removeChild(obstacle);
+        scoreCount += 10;
+        score.innerHTML = scoreCount;
+      }
+
+      if (birdBottom <= 60 || birdBottom === 470) {
+        clearInterval(timerId);
+        gameOver();
+      }
+    };
+    let timerId = setInterval(moveObstacle, 20);
+    if (!isGameOver) {
+      setTimeout(generateObstacle, 1000);
+    }
+
+    elementContainer.forEach(element => {
+      num = isOverlapping(bird, element);
+        if(num.overlapState) {
+          switch(num.number){
+            case 0: currentlyColliding = true;
+                    console.log("Dragon");
+                    gameOver();
+                    break;
+            case 1: currentlyColliding = true;
+                    element.mdiv.style.backgroundImage = "url('./obstacle/EaglePickup.gif')";
+                    console.log("Eagle");
+                    time += 7;
+                    break;
+            case 2: currentlyColliding = true;
+                    element.mdiv.style.backgroundImage = "url('./obstacle/')";        
+                    console.log("Foot");
+                    scoreCount += 25;
+                    break;
+            case 3: currentlyColliding = true;
+                    element.mdiv.style.backgroundImage = "url('./obstacle/FlagPickup.gif')";        
+                    console.log("Flag");
+                    scoreCount += 25;
+                    break;
+            case 4: scoreCount += 75;
+                    currentlyColliding = true;
+                    element.mdiv.style.backgroundImage = "url('./obstacle/CloverPickup.gif')";
+                    console.log("Clover");
+                    break;
+            case 5: urrentlyColliding = true;
+                    time += 7;        
+                    element.mdiv.style.backgroundImage = "url('./obstacle/SirenPickup.gif')";
+                    console.log("Siren");
+                    time += 7;
+                    break;
+          }
+        } 
+      
+  
+    });
+
+  };
+
+  generateObstacle();
+
+  const ChangeBackground = () => {
+    let randomBackground = Math.floor(Math.random() * 4);
+    console.log(randomBackground);
+    sky.style.backgroundImage = `url('./backgrounds/${backgroundArray[randomBackground]}Static.png')`;
+    groundContainer.style.backgroundImage = `url('./backgrounds/${backgroundArray[randomBackground]}Ground.png')`;
+  };
+
+  let backgroundChange = setInterval(() => {
+    if (!isGameOver) {
+      ChangeBackground();
+    }
+  }, 20000);
+
+  const gameOver = () => {
+    clearInterval(gameTimerId);
+    clearInterval(backgroundChange);
+
+    bird.style.backgroundImage = "url(LeprechaunDead.png)"
+
+    isGameOver = true;
+    document.removeEventListener("keyup", control);
+  };
+
+  document.addEventListener("keyup", control);
+
+  const startTimer = () => {
+    let timerCounter = setInterval(() => {
+      if (time === 0) {
+        gameOver();
+        isGameOver = true;
+        clearInterval(timerCounter);
+      }
+      if (!isGameOver) {
+        time -= 1;
+        timer.innerHTML = time;
+      }
+    }, 1000);
+  };
+  startTimer();
+});
